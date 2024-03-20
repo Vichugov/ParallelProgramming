1dc7f2794fbcb6df0c952bdefb0b6be3b54da56e
Vichugov Alexey RIS-20-2b <73594794+Vichugov@users.noreply.github.com>
2024-03-20 15:17:10 +0500

    MPI code with static division matrix to parts
    Time of running ~8 s.

--- L5MPI.cpp
+++ L5MPI.cpp
@@ -103,91 +103,152 @@ int main()
 	int rank;
 	int commsize;
 	int msize = 1000;
+	int fsize;
 	
 	MPI_Init(NULL, NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &commsize);
-
 	MPI_Status status;
+
+	int psize = msize / commsize;
+	int remain = msize % commsize;
+	int startindex;
+	int finishindex;
+	double** matrix;
+	double* rightnum;
+	double* truesolutions;
+	double** fragment;
+	double* massiv;
+	double* rightpart;
+	chrono::steady_clock::time_point start;
 
 	if (rank == 0)
 	{
-			
-		double **matrix = new double*[msize];
-		double *rightnum = new double[msize];
+	
+		matrix = new double*[msize];
+		rightnum = new double[msize];
 
-		double* truesolutions = GenerateSLAU(matrix, rightnum, msize);
-		chrono::steady_clock::time_point start = chrono::steady_clock::now();
+		truesolutions = GenerateSLAU(matrix, rightnum, msize);
+		start = chrono::steady_clock::now();
 
-		int nocode = -1;
-		int psize = 0;
-		int remains = 0;
-		int startindex = 0;
-		double* mainstring;
-		double* massiv;
-		double* rightpart;
-
-		for (int a = 0; a < msize - 1; ++a)
+		for (int a = 1; a < commsize; ++a)
 		{
-			psize = (msize - 1 - a) / commsize;
-			remains = (msize - 1 - a) % commsize;
-			mainstring = PartOfMatrixToMassiv(matrix, msize, a, 1);
+			massiv = PartOfMatrixToMassiv(matrix, msize, a * psize + remain, psize);
+			rightpart = TakePartOfMassiv(rightnum, a * psize + remain, psize);
+			MPI_Send(massiv, msize * psize, MPI_DOUBLE, a, 0, MPI_COMM_WORLD);
+			MPI_Send(rightpart, psize, MPI_DOUBLE, a, 1, MPI_COMM_WORLD);
+		}
 
-			for (int b = 1; b < commsize; ++b)
+		fsize = psize + remain;
+		fragment = new double*[fsize];
+		rightpart = new double[fsize];
+		for (int a = 0; a < fsize; ++a)
+		{
+			fragment[a] = new double[msize];
+			rightpart[a] = rightnum[a];
+			for (int b = 0; b < msize; ++b)
 			{
-				if (psize)
-				{
-					startindex = a + 1 + remains + (b * psize);
-					massiv = PartOfMatrixToMassiv(matrix, msize, startindex, psize);
+				fragment[a][b] = matrix[a][b];
+			}
+		}
+		startindex = 0;
+		finishindex = fsize;
+	}
+	else
+	{
+		fsize = psize;
+		massiv = new double[msize * fsize];
+		rightpart = new double[fsize];
+		MPI_Recv(massiv, msize * fsize, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(rightpart, fsize, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
 
-					MPI_Send(&a, 1, MPI_INT, b, 0, MPI_COMM_WORLD);
-					MPI_Send(mainstring, msize, MPI_DOUBLE, b, 1, MPI_COMM_WORLD);
-					MPI_Send(massiv, msize * psize, MPI_DOUBLE, b, 2, MPI_COMM_WORLD);
-					//delete[] massiv;
-				}
-				else
-				{
-					MPI_Send(&nocode, 1, MPI_INT, b, 0, MPI_COMM_WORLD);
-				}
+		fragment = new double*[fsize];
+		for (int a = 0; a < fsize; ++a)
+		{
+			fragment[a] = new double[msize];
+		}
+		MassivToPartOfMatrix(fragment, massiv, msize, 0, fsize);
+		startindex = (psize + remain) + (psize * (rank - 1));
+		finishindex = startindex + psize;
+	}
 
+	double* mainstring = new double[msize];
+	double rightelement;
+	int sender = 0;
+	double multiplier;
+
+	for (int a = 0; a < msize; ++a)
+	{
+		if (a >= startindex && a < finishindex)
+		{
+			for (int b = rank + 1; b < commsize; ++b)
+			{
+				MPI_Send(fragment[a - startindex], msize, MPI_DOUBLE, b, 2, MPI_COMM_WORLD);
+				MPI_Send(&rightpart[a - startindex], 1, MPI_DOUBLE, b, 3, MPI_COMM_WORLD);
 			}
 
-			//delete[] mainstring;
-			double multiplier = 0;
-		    for (int b = a + 1; b < (a + psize + remains + 1); ++b)
-		    {
-		        multiplier = matrix[b][a] / matrix[a][a];
-		        for (int c = a; c < msize; ++c)
-		        {
-		       		matrix[b][c] += matrix[a][c] * multiplier * -1;
-		        }
-		        rightnum[b] += rightnum[a] * multiplier * -1;
-		    }
-
-			for (int b = (a + psize + remains + 1); b < msize; ++b)
+			for (int b = a - startindex + 1; b < fsize; ++b)
 			{
-				multiplier = matrix[b][a] / matrix[a][a];
-		        rightnum[b] += rightnum[a] * multiplier * -1;
-			}
-
-			if (psize)
-			{
-				for (int b = 1; b < commsize; ++b)
+				multiplier = fragment[b][a] / fragment[a - startindex][a];
+				for (int c = a; c < msize; ++c)
 				{
-					startindex = a + 1 + remains + (b * psize);
-					MPI_Recv(massiv, msize * psize, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, &status);
-
-					MassivToPartOfMatrix(matrix, massiv, msize, startindex, psize);
-					//delete[] massiv;
+					fragment[b][c] += fragment[a - startindex][c] * multiplier * -1;
+				}
+				rightpart[b] += rightpart[a - startindex] * multiplier * -1;
+			}
+		}
+		else
+		{
+			if (a < startindex)
+			{
+				sender = (a - remain) / psize;
+				MPI_Recv(mainstring, msize, MPI_DOUBLE, sender, 2, MPI_COMM_WORLD, &status);
+				MPI_Recv(&rightelement, 1, MPI_DOUBLE, sender, 3, MPI_COMM_WORLD, &status);
+				
+				for (int b = 0; b < fsize; ++b)
+				{
+					multiplier = fragment[b][a] / mainstring[a];
+					for (int c = a; c < msize; ++c)
+					{
+						fragment[b][c] += mainstring[c] * multiplier * -1;
+					}
+					rightpart[b] += rightelement * multiplier * -1;
 				}
 			}
 		}
+	}
 
+	if (rank == 0)
+	{
+		for (int a = 0; a < fsize; ++a)
+		{
+			rightnum[a] = rightpart[a];
+			for (int b = 0; b < msize; ++b)
+			{
+				matrix[a][b] = fragment[a][b];
+			}
+			delete[] fragment[a];
+		}
+
+		rightpart = new double[psize];
+
+		for (int a = 1; a < commsize; ++a)
+		{
+			MPI_Recv(massiv, msize * psize, MPI_DOUBLE, a, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(rightpart, psize, MPI_DOUBLE, a, 1, MPI_COMM_WORLD, &status);
+			MassivToPartOfMatrix(matrix, massiv, msize, fsize + (a - 1) * psize, psize);
+			for (int b = 0; b < psize; ++b)
+			{
+				rightnum[fsize + (a - 1) * psize + b] = rightpart[b];
+			}
+		}
+
+		double sum = 0; 
 		double *solutions = new double[msize];
 		solutions[msize - 1] = rightnum[msize - 1] / matrix[msize - 1][msize - 1];
 		for (int a = msize - 2; a >= 0; --a)
 		{
-		    double sum = rightnum[a];
+		    sum = rightnum[a];
 		    for (int b = msize - 1; b > a; --b)
 		    {
 		        sum -= matrix[a][b] * solutions[b];
@@ -200,68 +261,33 @@ int main()
 
 		cout << "Time of running: " << calculatetime.count() << endl;
 		if (TestSolves(truesolutions, solutions, msize)) cout << "True. " << endl;
-		else cout << "False. ";
-		
-		for (int a = 0; a < msize; ++a) delete[] matrix[a];
+		else cout << "False. " << endl;
+
+		for (int a = 0; a < msize; ++a)
+		{
+			delete[] matrix[a];
+		}
+
 		delete[] matrix;
 		delete[] rightnum;
-		delete[] truesolutions;
 		delete[] solutions;
 	}
 	else
 	{
-		int index = 0;
-		int kstrings = 0;
-		int masslength = 0;
-		double* mainstring;
-		double* massiv;
-		double** matrix;
+		massiv = PartOfMatrixToMassiv(fragment, msize, 0, psize);
+		MPI_Send(massiv, msize * psize, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(rightpart, psize, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
 
-		for (int a = 0; a < msize - 1; ++a)
+		for (int a = 0; a < psize; ++a)
 		{
-			MPI_Recv(&index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-
-			if (index >= 0)
-			{
-				MPI_Probe(0, 2, MPI_COMM_WORLD, &status);
-				MPI_Get_count(&status, MPI_DOUBLE, &masslength);
-
-				mainstring = new double[msize];
-				massiv = new double[masslength];
-				MPI_Recv(mainstring, msize, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
-				MPI_Recv(massiv, masslength, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, &status);
-
-				kstrings = masslength / msize;
-				matrix = new double*[kstrings];
-				for (int b = 0; b < kstrings; ++b)
-				{
-					matrix[b] = new double[msize];
-				}
-
-				MassivToPartOfMatrix(matrix, massiv, msize, 0, kstrings);
-				
-				double multiplier = 0;
-				for (int b = 0; b < kstrings; ++b)
-				{
-					multiplier = matrix[b][index] / mainstring[index];
-					for (int c = index; c < msize; ++c)
-					{
-						matrix[b][c] += mainstring[c] * multiplier * -1;
-					}
-				}
-				
-				delete[] mainstring;
-				delete[] massiv;
-				massiv = PartOfMatrixToMassiv(matrix, msize, 0, kstrings);
-				for (int b = 0; b < kstrings; ++b) delete[] matrix[b];
-				delete[] matrix;
-				
-				MPI_Send(massiv, masslength, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-				delete[] massiv;
-			}
-		}	
+			delete[] fragment[a];
+		}
 	}
-	
+
+	delete[] fragment;
+	delete[] rightpart;
+	delete[] massiv;
+			
 	MPI_Finalize();
 	return 0;
 }
